ac5c99215faa2e2018982776f9580ee442b5a534
Sithija Ranaraja <sithija.vihanga28@gmail.com>
2024-08-17 18:06:22 +0530

    Removed debugging frames

--- src/voyager_localization/launch/local_localization.launch.py
+++ src/voyager_localization/launch/local_localization.launch.py
@@ -1,9 +1,26 @@
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 import os
 
 def generate_launch_description():
+
+    use_imu_republisher_arg = DeclareLaunchArgument(
+        "use_imu_republisher",
+        default_value= "False"
+    )
+
+    use_ekf_tf_publisher_arg = DeclareLaunchArgument(
+        "use_ekf_tf_publisher",
+        default_value= "False"
+    )
+
+    use_imu_republisher   = LaunchConfiguration("use_imu_republisher")
+    use_ekf_tf_publisher  = LaunchConfiguration("use_ekf_tf_publisher")
+
 
     static_transform_publisher = Node(
         package="tf2_ros",
@@ -12,6 +29,7 @@ def generate_launch_description():
                    "--qx", "1", "--qy", "0", "--qz", "0", "--qw", "0",
                    "--frame-id", "base_footprint_ekf",
                    "--child-frame-id", "imu_link_ekf"],
+        condition= IfCondition(use_ekf_tf_publisher)
     )
 
     robot_localization = Node(
@@ -25,10 +43,13 @@ def generate_launch_description():
 
     imu_republisher = Node(
         package="voyager_localization",
-        executable="imu_republisher"
+        executable="imu_republisher",
+        condition= IfCondition(use_imu_republisher)
     )
 
     return LaunchDescription([
+        use_imu_republisher_arg,
+        use_ekf_tf_publisher_arg,
         static_transform_publisher,
         robot_localization,
         imu_republisher  
